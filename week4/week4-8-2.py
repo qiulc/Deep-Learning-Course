5288a3a710d4fc11138925f7b4319007efb29c3d
仇浪聪 <972132031@qq.com>
2020-04-04 13:54:23 +0800

    修改tensorflow的错题

--- week4/week4-8-2.py
+++ week4/week4-8-2.py
@@ -6,33 +6,15 @@ x = tf.constant([64.3, 99.6, 145.45, 63.75, 135.46, 92.85, 86.97, 144.76, 59.3, 
 y = tf.constant([62.55, 82.42, 132.62, 73.31, 131.05, 86.57, 85.49, 127.44, 55.25, 104.84])
 
 n = len(x)
-
-Numerator = tf.constant(0.)
-Denominator = tf.constant(0.)
-
-N_tem1 = tf.constant(0.)
-N_tem2 = tf.constant(0.)
-D_tem1 = tf.constant(0.)
-D_tem2 = tf.constant(0.)
-
-y_sum = tf.reduce_sum(y)
 x_sum = tf.reduce_sum(x)
+y_sum = tf.reduce_sum(y)
+Numerator = n * tf.reduce_sum(tf.multiply(x, y)) - tf.reduce_sum(y_sum * x)
+Denominator = n * tf.reduce_sum(tf.pow(x, 2)) - tf.pow(x_sum, 2)
 
-D_tem2 = tf.pow(x_sum, 2)
 
-for i in range(n):
-    N_tem1 += x[i] * y[i]
-    N_tem2 += x[i] * y_sum
-    D_tem1 += tf.pow(x[i], 2)
-    
-N_tem1 = N_tem1 * n
-D_tem1 = D_tem1 * n
-
-Numerator = N_tem1 - N_tem2
-Denominator = D_tem1 - D_tem2
 w = Numerator / Denominator
 
-b = (y_sum - w * tf.reduce_sum(x)) / n
+b = (y_sum - w * x_sum) / n
 
 print("w: {}".format(w))
 print("b: {}".format(b))
